1554bb9a7c4c1abcd2b8c7b6a40cb63ed4c646af
thirsha5 <3sha0104@gmail.com>
2022-10-26 08:56:04 -0400

    Display Coffee Products List

--- coffee-shop/src/app/Store/Reducer/product.reducers.ts
+++ coffee-shop/src/app/Store/Reducer/product.reducers.ts
@@ -14,26 +14,3 @@ export const productReducer = createReducer(
   initialState,
   on(getProductsSuccess, (state, {products}) =>[...products]),   
 );
-
-function mockMovies(): Products[] {
-    const movie = new Products(
-      1,
-      'test',    
-    );
-    movie.id = 1;
-  
-    const movie1 = new Products(
-        2,
-        'test'
-    );
-    movie1.id = 2;
-  
-    const movie2 = new Products(
-        3,
-        'test'
-    );
-    movie2.id = 3;
-  
-    const movies = [movie, movie1, movie2];
-    return movies;
-  }
